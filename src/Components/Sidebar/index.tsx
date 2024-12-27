600736ac56e1e71c16a7c2b84c96f37bbcb27dfa
yommexg <hydrokidb@gmail.com>
2024-12-27 20:13:21 +0000

    Navbar updated

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -6,8 +6,8 @@ const logoImage = new URL("../../assets/logo/white-logo.png", import.meta.url)
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="w-[250px] bg-[#010D14] fixed top-0 bottom-0 py-10 px-8">
-      <div className="flex gap-2 items-center mb-16">
+    <div className="w-[250px] bg-[#010D14] fixed top-0 bottom-0 p-8">
+      <div className="flex gap-2 items-center mb-10">
         <img
           src={logoImage}
           alt="logo"
@@ -18,7 +18,7 @@ const Sidebar: React.FC = () => {
         </h1>
       </div>
       <p className="text-[#FCFCFC] font-[550] text-xs">MENU</p>
-      <div className="mt-10 flex flex-col gap-10">
+      <div className="mt-6 flex flex-col gap-8">
         {SidebarData.map((item) => (
           <NavLink
             to={item.link}
---
+++ src/utils/avatars.ts
@@ -0,0 +1,27 @@
+const avatar1 = new URL("../assets/avatars/1.png", import.meta.url).href;
+const avatar2 = new URL("../assets/avatars/2.png", import.meta.url).href;
+const avatar3 = new URL("../assets/avatars/3.png", import.meta.url).href;
+const avatar4 = new URL("../assets/avatars/4.png", import.meta.url).href;
+const avatar5 = new URL("../assets/avatars/5.png", import.meta.url).href;
+const avatar6 = new URL("../assets/avatars/6.png", import.meta.url).href;
+const avatar7 = new URL("../assets/avatars/7.png", import.meta.url).href;
+const avatar8 = new URL("../assets/avatars/8.png", import.meta.url).href;
+const avatar9 = new URL("../assets/avatars/9.png", import.meta.url).href;
+const avatar10 = new URL("../assets/avatars/10.png", import.meta.url).href;
+const avatar11 = new URL("../assets/avatars/11.png", import.meta.url).href;
+const avatar12 = new URL("../assets/avatars/12.png", import.meta.url).href;
+
+export const AvatarList = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+  avatar10,
+  avatar11,
+  avatar12,
+];
